b1459009131a444d53ca8e7252494d4fc6028baa
Ilya Shchekotov <ilya.shchekotov@mail.ru>
2023-05-11 23:18:49 +0200

    Implemented jar packaging

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,25 @@ plugins {
 }
 
 group = "de.mgm.inf.mgmgame"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+tasks {
+    getByName<Test>("test") {
+        useJUnitPlatform()
+    }
+
+    getByName<Jar>("jar") {
+        manifest {
+            attributes("Main-Class" to "de.mgm.inf.mgmgame.Main")
+        }
+    }
 }
